39cce25ce647c64d2029ec3285c5fde34b369fca
EvaBao <2548696126@qq.com>
2018-10-10 19:55:18 +0800

    MinWeather

---
+++ app/src/main/java/cn/edu/pku/ss/eva_bao/minweather/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.pku.ss.eva_bao.minweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
